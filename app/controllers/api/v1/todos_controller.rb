b7a709fa625f75b53f6ebcad312a96845a6007fa
xingwei <weixing1985@gmail.com>
2017-04-16 22:56:36 -0700

    xw - more controller tests

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,10 +1,27 @@
 module Api
   module V1
     class TodosController < ApplicationController
-      def index
-        @todos = Todo.all
+      before_action :find_todo, only: [:show, :destroy]
 
-        render json: @todos, status: :ok
+      def index
+        todos = Todo.all
+
+        render json: todos, status: :ok
+      end
+
+      def show
+        render json: @todo, status: :ok
+      end
+
+      def destroy
+        @todo.destroy
+        head :no_content
+      end
+
+      private
+
+      def find_todo
+        @todo = Todo.find(params[:id])
       end
     end
   end
--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Todos API', type: :request do
   let!(:todos) { create_list(:todo, 10) }
+  let(:todo_id) { todos.first.id }
 
   describe 'GET /api/v1/todos' do
     before { get '/api/v1/todos' }
@@ -12,6 +13,42 @@ RSpec.describe 'Todos API', type: :request do
 
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /api/v1/todos/:id' do
+    context 'when :id is valid' do
+      before { get "/api/v1/todos/#{todo_id}" }
+
+      it 'returns todo' do
+        expect(json['id']).to eq(todo_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when :id is invalid' do
+      # Todo
+    end
+  end
+
+  describe 'DELETE /api/v1/todos/:id' do
+    context 'when :id is valid' do
+      before { delete "/api/v1/todos/#{todo_id}" }
+
+      it 'removes todo with :id from database' do
+        expect(Todo.all.size).to eq(9)
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+
+    context 'when :id is invalid' do
+      # Todo
     end
   end
 end

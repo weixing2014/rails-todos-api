e8aa218e0bdabf8cf6dff4446959caf7df5de238
xing_wei <xing_wei@ml007188xwei.hq.us.airgc.net>
2017-04-20 17:24:11 -0700

    xw - wip: jwt authentication

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class TodosController < ApplicationController
+      before_action :authenticate_user
       before_action :find_todo, only: [:show, :update, :destroy]
 
       def index

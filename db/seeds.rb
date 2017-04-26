1ad749639ee6559231b5040d933d12225fdd559a
xing_wei <xing_wei@ml007188xwei.lan>
2017-04-25 22:56:36 -0700

    xw - add data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(name: 'Xing Wei', email: 'xing@wei.com', password: '123456', password_confirmation: '123456')
+
+50.times do
+  todo = Todo.create(title: Faker::Lorem.word, created_by: User.first.id)
+  todo.items.create(name: Faker::Lorem.word)
+end
